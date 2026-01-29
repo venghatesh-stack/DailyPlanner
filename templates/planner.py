60028cd56662fc9a96a0e04da6ff551a6e0d7fa8
venghatesh-stack <venghatesh@gmail.com>
2026-01-29 21:45:08 +0530

    Render timed tasks only as floating blocks and remove slot duplication

--- templates/planner.py
+++ templates/planner.py
@@ -112,19 +112,22 @@ PLANNER_TEMPLATE = """
         <div class="task-layer">
           {% for task in tasks %}
             <div
-                class="task-block"
-                style="top: {{ task.top_px }}px; height: {{ task.height_px }}px;
+              class="task-block"
+              style="
+                top: {{ task.top_px }}px;
+                height: {{ task.height_px }}px;
                 --col-index: {{ task.col }};
-                --col-count: {{ task.col_count }};"
-              >
-                <div class="task-title">{{ task.text }}</div>
-                <div class="task-time">
-                  {{ task.start_time }} – {{ task.end_time }}
-                </div>
+                --col-count: {{ task.col_count }};
+              "
+            >
+              <div class="task-title">{{ task.text }}</div>
+              <div class="task-time">
+                {{ task.start_time }} – {{ task.end_time }}
               </div>
-
+            </div>
           {% endfor %}
         </div>
+
       </div>
 
 
