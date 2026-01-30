c7ec9236f34f51a59b5c1eece5b3628c085859f0
venghatesh-stack <venghatesh@gmail.com>
2026-01-30 09:59:45 +0530

    Update planner.js

--- static/planner.js
+++ static/planner.js
@@ -1,5 +1,5 @@
 
-const USE_TIMELINE_VIEW = true;
+const USE_TIMELINE_VIEW = false;
 const summaryModal = document.getElementById("summary-modal");
 const summaryContent = document.getElementById("summary-content");
 
