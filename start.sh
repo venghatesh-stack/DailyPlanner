cd4fa7d4b16446d1058f1274fb7156e22f4185c6
venghatesh-stack <venghatesh@gmail.com>
2026-02-21 21:06:39 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,8 +1,3 @@
 #!/usr/bin/env bash
 echo "Starting on port: $PORT"
-exec gunicorn app:app
---bind 0.0.0.0:$PORT
---workers 1
---threads 2
---timeout 120
---log-level debug
+exec gunicorn app:app --bind 0.0.0.0:$PORT --workers 1 --threads 2 --timeout 120 --log-level debug
